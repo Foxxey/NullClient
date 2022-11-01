f93ee9cf55b36e5524e1469c92c676b7735df5e0
CrafterL <68051476+CrafterlKrunker@users.noreply.github.com>
2022-11-01 21:05:58 +0100

    Delete app directory

--- app/features/features.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    keystrokes: require('./keystrokes.js'),
-    //skyColor: require('./skyColor.js')
-    menuTimer: require('./menuTimer.js'),
-    //quickJoin: require('./quickJoin.js'),
-    //badges: require('./badges.js'),
-}
